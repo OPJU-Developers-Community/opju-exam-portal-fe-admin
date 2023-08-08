20b547f5f2ab7a968e388c71c061b7de96e8e00a
subrat <subratj611@gmail.com>
2023-08-08 23:37:26 +0530

    minor changes

--- components/Login/LoginStyle.tsx
+++ components/Login/LoginStyle.tsx
@@ -10,6 +10,7 @@ export const LeftSection = styled("div")({
   flex: 1,
   display: "flex",
   alignItems: "center",
+  justifyContent: "center",
 });
 
 export const RightSection = styled("div")({
@@ -27,63 +28,61 @@ export const LoginBox = styled("div")({
 });
 
 export const Title = styled("h1")({
-    margin: 0,
-    padding: 0,
-    color: "#212529",
-    fontWeight: 600,
-  });
-  
-  export const SubTitle = styled("h3")({
-    margin: "1rem 0",
-    padding: 0,
-    color: "#343a40",
+  margin: 0,
+  padding: 0,
+  color: "#212529",
+  fontWeight: 600,
+});
+
+export const SubTitle = styled("h3")({
+  margin: "1rem 0",
+  padding: 0,
+  color: "#343a40",
+  fontWeight: 500,
+});
+
+export const InputField = styled(TextField)({
+  width: "100%",
+  margin: "1.13rem 0",
+
+  "& .MuiFormLabel-root": {
+    fontFamily: "Nunito",
     fontWeight: 500,
-  });
+    fontSize: 16,
 
-  export const InputField = styled(TextField)({
-    width: "100%",
-    margin: "1.13rem 0",
-  
-    "& .MuiFormLabel-root": {
-      fontFamily: "Nunito",
-      fontWeight: 500,
-      fontSize: 16,
-  
-      "&.Mui-focused": {
-        color: "#4361ee",
-      },
+    "&.Mui-focused": {
+      color: "#4361ee",
     },
-  
-    "& .MuiInputBase-root:after": {
-      borderBottom: "2px solid #4361ee",
-    },
-  
-    "& .MuiInputBase-root:hover": {
-      borderBottom: "none", // Remove the underline when the input is focused
-    },
-  
-    "& .MuiInputBase-input": {
-      fontFamily: "Nunito",
-      fontWeight: 500,
-      fontSize: 16,
-    },
-  });
+  },
 
-  export const LoginButton = styled(Button)({
-    width: "100%",
-    margin: "1rem 0",
+  "& .MuiInputBase-root:after": {
+    borderBottom: "2px solid #4361ee",
+  },
+
+  "& .MuiInputBase-root:hover": {
+    borderBottom: "none", // Remove the underline when the input is focused
+  },
+
+  "& .MuiInputBase-input": {
+    fontFamily: "Nunito",
+    fontWeight: 500,
+    fontSize: 16,
+  },
+});
+
+export const LoginButton = styled(Button)({
+  width: "100%",
+  margin: "1rem 0",
+  boxShadow: "none",
+  backgroundColor: "#4361ee",
+  textTransform: "capitalize",
+  fontFamily: "Nunito",
+  fontSize: 16,
+  fontWeight: 500,
+  padding: "0.65rem 0",
+
+  "&.MuiButtonBase-root:hover": {
     boxShadow: "none",
     backgroundColor: "#4361ee",
-    textTransform: "capitalize",
-    fontFamily: "Nunito",
-    fontSize: 16,
-    fontWeight: 500,
-    padding: "0.65rem 0",
-
-    "&.MuiButtonBase-root:hover": {
-        boxShadow: "none",
-        backgroundColor: "#4361ee",
-    }
-  })
-    
-
+  },
+});
