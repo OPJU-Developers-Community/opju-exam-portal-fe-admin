4781b434e7cb9af7c25660d660918c3489047cde
Subrat kumar jena <subratj611@gmail.com>
2023-10-15 03:33:39 +0530

    refactor

--- types/index.type.ts
+++ types/index.type.ts
@@ -7,3 +7,9 @@ export interface EducationManagementModalPropType {
     isOpen: boolean,
     handleModalClose: () => void,
 }
+
+export interface stateType {
+    data: object | null,
+    apiStatus: 'idle' | 'loading' | 'success' | 'failed',
+    message: string
+}
---
+++ utils/constants.tsx
@@ -0,0 +1,85 @@
+import {
+  SerialNoIcon,
+  TableActionIcon,
+  TableEmailIcon,
+  TablePasswordIcon,
+  TableProfileIcon,
+  TableSubjectIcon,
+} from "@/atoms/Icons";
+
+type navLinkType = {
+  name: string;
+  path: string;
+};
+
+export const navLinks: navLinkType[] = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "User management",
+    path: "/user-management",
+  },
+  {
+    name: "Education management",
+    path: "/education-management",
+  },
+  {
+    name: "Report",
+    path: "/report",
+  },
+];
+
+// remove below array (part of component)
+export const userManagementUsers = [
+  "Question Setter",
+  "Question Verifier",
+  "Examiner",
+];
+
+export const userManagementTabs = [
+  {
+    query: "all",
+    value: "All",
+  },
+  {
+    query: "question_setter",
+    value: "Question Setter",
+  },
+  {
+    query: "question_verifier",
+    value: "Question Verifier",
+  },
+  {
+    query: "examiner",
+    value: "Examiner",
+  },
+];
+
+export const userManagementTableHeadCells = [
+  {
+    label: "S.no",
+    icon: <SerialNoIcon />,
+  },
+  {
+    label: "User name",
+    icon: <TableProfileIcon />,
+  },
+  {
+    label: "Email",
+    icon: <TableEmailIcon />,
+  },
+  {
+    label: "Password",
+    icon: <TablePasswordIcon />,
+  },
+  {
+    label: "Subjects access",
+    icon: <TableSubjectIcon />,
+  },
+  {
+    label: "Actions",
+    icon: <TableActionIcon />,
+  },
+];
